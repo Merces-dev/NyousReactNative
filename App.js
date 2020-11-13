2a44004ca66ed43bc63068a03d5f324fb8c7d9b6
Merces-dev <giovanitjs013@gmail.com>
2020-11-12 21:56:58 -0300

    Bibliotecas Instaladas

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Fuck GRMT</Text>
+      <Text>Nyous</Text>
       <StatusBar style="auto" />
     </View>
   );
